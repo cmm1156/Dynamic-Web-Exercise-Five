afe5163951e56fea4d381ed6d24107daf37e2459
Christopher Mancini <cmm1156@nyu.edu>
2020-11-24 14:28:50 -0500

    routes added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route, BrowserRouter as Router, Redirect } from 'react-router-dom';
 import './App.css';
 
+import Login from "./containers/Login";
+import CreateAccount from from "./containers/CreateAccount";
+import UserProfile from "./containers/UserProfile"
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  return <div className="App">
+    <Router>
+      <Route exact path="/login">
+        <Login />
+      </Route>
+      <Route exact path="/create-account">
+        <CreateAccount />
+      </Route>
+      <Route exact pth="/">
+        <UserProfile />
+      </Route>
+    </Router>
+  </div>
 
 export default App;
